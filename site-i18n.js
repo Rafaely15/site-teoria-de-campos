b801e96500439eb093abc673f18d39517d240c53
Ana rafaely <rafaelymedeiros2009@gmail.com>
2026-07-28 16:55:49 -0300

    salvando alterações

--- site-i18n.js
+++ site-i18n.js
@@ -37,7 +37,6 @@ window.CAMPOSGRAV_COMMON = {
   pt: {
     navHome: 'Início',
     navAbout: 'Sobre',
-    navProjects: 'Projetos',
     navPubs: 'Publicações',
     navPeople: 'Colaboradores',
     navNews: 'Notícias',
@@ -64,7 +63,6 @@ window.CAMPOSGRAV_COMMON = {
   en: {
     navHome: 'Home',
     navAbout: 'About',
-    navProjects: 'Projects',
     navPubs: 'Publications',
     navPeople: 'People',
     navNews: 'News',
